eacc68c87907b9d0d3327a0b1898df940f22d1d3
MikiAlex <18516340862@163.com>
2017-12-09 12:44:24 +0800

    commit old

--- src/camera/camera.ts
+++ src/camera/camera.ts
@@ -1,3 +1,15 @@
-export class camera{
+export class camera {
+  lookAtX: number;
+  lookAtY: number;
+
+  constructor() {
+
+  }
+
+  lookAt(x: number, y: number) {
+    this.lookAtX = x;
+    this.lookAtY = y;
+  }
+
 
 }
