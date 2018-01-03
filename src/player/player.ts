a10b5db8315fb626c42d7a12836ccb4ac06554ba
mikialex <18516340862@163.com>
2018-01-03 23:29:11 +0800

    change player control way

--- src/player/player.ts
+++ src/player/player.ts
@@ -5,19 +5,18 @@ import { VoxGame } from "../core/vox-game";
 import { VoxEvent } from "../core/event";
 
 export class Player extends ReactiveBase {
-  constructor(game:VoxGame) {
+  constructor(game: VoxGame) {
     super(game);
     this.on('keyactive', (e: VoxEvent) => {
       console.log('active')
       if (e.payload.keyType === 'ArrowUp') {
-        this.vy =   - 5;
+        this.vy = Player.vAddReverse(this.vy)
       } else if (e.payload.keyType === 'ArrowDown') {
-        this.vy =  + 5;
+        this.vy = Player.vAdd(this.vy)
       } else if (e.payload.keyType === 'ArrowLeft') {
-        this.vx =   - 5;
-      }else if (e.payload.keyType === 'ArrowRight') {
-        // this.vx =  Player.vAdd(this.vx);
-        this.vx =  +5;
+        this.vx = Player.vAddReverse(this.vx);
+      } else if (e.payload.keyType === 'ArrowRight') {
+        this.vx =  Player.vAdd(this.vx)
       }
     })
   }
@@ -26,12 +25,18 @@ export class Player extends ReactiveBase {
     // this.vx = 1;
   }
 
+  static vPrimary = 15;
+
   x = 40;
   y = 20;
   vx = 1;
   vy = 0;
 
-  tick(timeSpeed:number) {
+  get vSquare(){
+    return this.vx*this.vx+this.vy*this.vy;
+  }
+
+  tick(timeSpeed: number) {
     this.vx = Player.vDecay(this.vx);
     this.vy = Player.vDecay(this.vy);
     this.x = this.x + this.vx * timeSpeed;
@@ -39,16 +44,24 @@ export class Player extends ReactiveBase {
   }
 
   static vAdd(v: number) {
-    if (v < 5) {
+    if (v < Player.vPrimary) {
       return v + 2;
-    } else {
-      return v;
+    } else if (v>=Player.vPrimary) {
+      return Player.vPrimary;
+    }
+  }
+
+  static vAddReverse(v: number) {
+    if (v > -Player.vPrimary) {
+      return v - 2;
+    } else if (v<=-Player.vPrimary) {
+      return -Player.vPrimary;
     }
   }
 
   static vDecay(v: number) {
     // return 0;
-    let decayRate = 0.5;
+    let decayRate = 1;
     if (v > 0) {
       if (v - decayRate < 0) {
         return 0
@@ -67,15 +80,15 @@ export class Player extends ReactiveBase {
   }
 
   draw(renderer: Canvas2dRenderer, x: number, y: number) {
-    renderer.fillCircle('#f55', this.x+x, this.y+y, 5);
+    renderer.fillCircle('#f55', this.x + x, this.y + y, 5);
   }
 
 
-  pointTest(x:number,y:number) {
+  pointTest(x: number, y: number) {
     return false;
   }
 
-  dispose(){
+  dispose() {
 
   }
 }
